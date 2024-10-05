7c6165925e65b255a783a556fbdc358fb62dab4e
prajwal2303y <prajwalkumarkori2@gmail.com>
2024-10-05 11:51:45 +0530

    i am git repository

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// alert('no holiday for gandi jayanthi');
+// document.write('no holiday for gandi jayanthi');
+// let sum=4**4;
+// console.log(sum)
+let num=673148;
+if(condition){
+    console.lag('even number')
+}
+else{
+    console.log('odd number')
+}
